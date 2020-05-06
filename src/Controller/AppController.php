6bf442739e7c9b12a243a638a7a6b54b2d61bd58
agorw <58491349+agorw@users.noreply.github.com>
2020-05-06 11:22:57 +0200

    v0
    mater git

---
+++ src/Controller/AppController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class AppController extends AbstractController
+{
+    /**
+     *
+     * @Route("/")
+     * @Route("/index.php")
+     */
+    public function index()
+    {
+        return $this->render("app\index.html.twig");
+    }
+}
