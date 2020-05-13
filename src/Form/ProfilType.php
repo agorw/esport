cf8dfc153ada4768bddd4f02dc5993dab8dd67df
agorw <58491349+agorw@users.noreply.github.com>
2020-05-13 17:08:14 +0200

    profil j1
    profil j1

---
+++ src/Form/ProfilType.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Profil;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ProfilType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('nom')
+            ->add('prenom')
+            ->add('date_nee')
+            ->add('adress')
+            ->add('ville')
+            ->add('pays')
+            ->add('code_postal');
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Profil::class,
+        ]);
+    }
+}
