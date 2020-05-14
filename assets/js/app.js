bc80045216dff825d6ad0addfb6d2e6b6d66f21e
Mike-Jha <jhary57@gmail.com>
2020-05-14 12:54:47 +0200

    modif menu
    - Modif du visuel
    - Ajout de l'animation "slide"
    - (en attente) Modif responsive

--- assets/js/app.js
+++ assets/js/app.js
@@ -19,6 +19,14 @@ console.log("**********( <  )*******************");
 console.log("***********// \\\\**** BY AgorW******");
 console.log("***********************************");
 
-$("body").click(function () {
-  console.log("jquery");
+// Lorsqu'on clique sur le bouton menu, donne/enleve la classe "active" à sidebar
+// Cela permet de la faire apparaitre ou disparaitre
+$("#sidebarCollapse").click(function () {
+  $("#sidebar").toggleClass('active');
+  $("#sidebarCollapse").hide();
+});
+
+$("#btn-return").click(function () {
+  $("#sidebar").toggleClass('active');
+  $("#sidebarCollapse").show()
 });
